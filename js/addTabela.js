0aa66dbec6a44a2c062cd2cc347732ae1bc5cc6f
João Cshunderlick Neto <joaocshunderlick26@gmail.com>
2019-11-23 16:56:26 -0300

    Ajustes no layout

--- js/addTabela.js
+++ js/addTabela.js
@@ -1,33 +1,48 @@
-$(document).ready(function(){
+// Caso usemos o storage essa variavel não existirá mais.
+var qtdProdutos = 0;
 
-  $('.money').mask('000.000.000.000.000.00', {reverse: true});
-  $("#add-produto").bind("click", gerarTabela);
+$(document).ready(function () {
+
+  $('.money').mask('000.000.000.000.000,00', { reverse: true });
+  $('#add-produto').on('click', gerarTabela);
 
 });
 
-  function clear(){
-    $(".clear",).val("");
-  }
+function clear() {
+  $(".clear").val("");
+}
 
 
-  function valorTotal(){
-    var vt = parseFloat($('#valor-total').text());
-    var v =  (vt + $('#qtd-produto').val() * $('#preco-unitario-produto').val()).toFixed(2);
-    $('#valor-total').text(v); 
-  };
+function valorTotal(qtd, preco) {
+  var t = parseFloat($('#valor-total').text());
+  var v = (vt + qtd * preco);
+  $('#valor-total').text(v);
+};
 
-  function gerarTabela(){
+function gerarTabela() {
+  qtdProdutos++;
+  var qtdProduto = $('#qtd-produto').val();
+  var precoU = parseFloat($('#preco-unitario-produto').val()).toFixed(2);
 
-    $conteudo = "<tr class='row'>" +
-        "<td class='col-7'>" + $('#nome-produto').val() + "</th>" +
-        "<td class='col-2 text-center'>" + $('#qtd-produto').val() + "</th>" +
-        "<td class='col-2'>R$ " + parseFloat($('#preco-unitario-produto').val()).toFixed(2) + "</th>" +
-        "<td class='col-1 text-center'><button id='gerarIdParaCadaitem' class='btn btn-outline-danger btn-sm circle .btnExcluir'><i class='fas fa-trash'></i></button></th>" +
-        "</tr>";
-        
-    $('#tBody').append($conteudo);
-    valorTotal();
-    clear();
-  }
+  conteudo = "<tr class='row' id='" + qtdProdutos + "'>" +
+    "<td class='col-7'>" + $('#nome-produto').val() + "</th>" +
+    "<td class='col-2 text-center'>" + qtdProduto + "</th>" +
+    "<td class='col-2'>R$ " + precoU + "</th>" +
+    "<td class='col-1 text-center'><button value='" + qtdProdutos + "' class='btn btn-outline-danger btn-sm circle btnExcluir'><i class='fas fa-trash'></i></button></th>" +
+    "</tr>";
 
- 
+  $('#tBody').append(conteudo);
+  $('.btnExcluir').click(function () {
+    id = $(this).val()
+    $('tr').each(function () {
+      if ($(this).attr('id') == id) {
+        $(this).remove();
+      }  
+    })
+  })
+  valorTotal(qtdProduto, precoU);
+  clear();
+}
+
+
+
